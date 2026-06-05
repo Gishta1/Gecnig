47b79c07c2c8bf62b7e0aec439b2c6d07474d2f0
Gishta1 <Gishta321@gmail.com>
2026-06-05 15:59:49 +0300

    aboba

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,33 +1,37 @@
-const cacheName = "ООО Несуществующая компания-Gecnig-666";
+const cacheName = "Gecnig-1.0";
 const contentToCache = [
-    "Build/Gencnig Web NotGit.loader.js",
-    "Build/Gencnig Web NotGit.framework.js.gz",
-    "Build/Gencnig Web NotGit.data.gz",
-    "Build/Gencnig Web NotGit.wasm.gz",
+    "Build/Gecnig.loader.js",
+    "Build/Gecnig.framework.js",
+    "Build/Gecnig.data",
+    "Build/Gecnig.wasm",
     "TemplateData/style.css"
-
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
     e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil((async function () {
+        const keys = await caches.keys();
+        await Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)));
     })());
 });
 
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+        let response = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (response) { return response; }
+        response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
     })());
 });
