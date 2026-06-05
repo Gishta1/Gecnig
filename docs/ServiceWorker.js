3c3c31eb1d88ba869c5779ddabb0abe2f9e5de81
Gishta1 <Gishta321@gmail.com>
2026-06-05 15:48:33 +0300

    aboba

---
+++ docs/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ООО Несуществующая компания-Gecnig-666";
+const contentToCache = [
+    "Build/Gencnig Web NotGit.loader.js",
+    "Build/Gencnig Web NotGit.framework.js.gz",
+    "Build/Gencnig Web NotGit.data.gz",
+    "Build/Gencnig Web NotGit.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
